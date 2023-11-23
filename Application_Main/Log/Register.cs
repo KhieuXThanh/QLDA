c131f857ef4865c88c4d9155bcf934b15f5e2d5b
KhieuXThanh <AOGKTashU@gmail.com>
2023-11-24 01:54:35 +0700

    QLDAn

---
+++ Application_Main/Log/Register.cs
@@ -0,0 +1,60 @@
+﻿using QuanLyDuAnBDS.Log;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Application
+{
+
+    public partial class Register : Form
+    {
+
+        public Register(string textbox)
+        {
+            InitializeComponent();
+            this.txt_TenDn.Text = textbox;
+        }
+
+        private void btn_Dn_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Login lg = new Login(txt_TenDn.Text);
+            lg.ShowDialog();
+            this.Close();
+        }
+        public bool KiemtraMk(string mk, string xnmk)
+        {
+            if (mk != xnmk)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void btn_Dk_Click(object sender, EventArgs e)
+        {
+            if (KiemtraMk(lb_Mk.Text, lb_XnMk.Text))
+            {
+                this.Hide ();
+                infor infor = new();
+                infor.ShowDialog();
+                infor.LayThongTin(lb_TenDn.Text,lb_Mk.Text);
+                this.Close();
+            }
+            else 
+            {
+                lb_TB.Show();
+            }
+        }
+
+
+    }
+}
---
+++ Application_Main/Log/infor.cs
@@ -0,0 +1,46 @@
+﻿using Application;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyDuAnBDS.Log
+{
+    public partial class infor : Form
+    {
+        private string tk;
+        private string mk;
+
+        public infor()
+        {
+            InitializeComponent();
+        }
+        public void LayThongTin(string tkc, string mkc)
+        {
+            tk = tkc; mk = mkc;
+
+        }
+
+        private void btn_Thoat_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Register rg = new(tk);
+            rg.ShowDialog();
+            this.Close();
+        }
+
+        private bool CheckThongtin()
+        {
+            return false;
+        }
+        private void btn_DK_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
